ded845ceb96b3a72e317de4ad14d84db6b5b8210
Er. Anmol Singh <63386088+AnmolSingh9264@users.noreply.github.com>
2026-04-28 16:10:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,26 +3,28 @@
 echo "Starting virtual display..."
 Xvfb $DISPLAY -screen 0 $RESOLUTION &
 
-sleep 2
+sleep 3
 
 echo "Starting window manager..."
 fluxbox &
 
-echo "Starting VNC server..."
+sleep 3
+
+echo "Starting VNC..."
 x11vnc -display $DISPLAY -nopw -forever -shared &
 
-echo "Starting noVNC on port $PORT ..."
+echo "Starting noVNC..."
 websockify --web=/usr/share/novnc ${PORT:-6080} localhost:5900 &
 
-sleep 5
+# 🔥 Give everything time to stabilize
+sleep 10
 
-echo "Launching browser..."
+echo "Launching Firefox..."
 
-chromium-browser \
-  --no-sandbox \
-  --disable-dev-shm-usage \
-  --disable-gpu \
-  --start-maximized \
+DISPLAY=:1 firefox \
+  --no-remote \
+  --new-instance \
+  --kiosk \
   https://rarestudy.in
 
 wait
