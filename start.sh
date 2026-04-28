74392f96a41fc7c0357b54ed972c125e378f5561
Er. Anmol Singh <63386088+AnmolSingh9264@users.noreply.github.com>
2026-04-28 18:26:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Cleaning stale X locks (important for Render restarts)..."
+echo "Cleaning stale X locks..."
 rm -f /tmp/.X1-lock /tmp/.X11-unix/X1
 
-echo "Starting virtual display..."
+echo "Starting Xvfb..."
 Xvfb :1 -screen 0 1280x720x24 -ac +extension GLX +render -noreset &
 export DISPLAY=:1
 
@@ -15,27 +15,22 @@ fluxbox >/dev/null 2>&1 &
 
 sleep 2
 
-echo "Starting VNC server..."
-x11vnc -display :1 \
-  -nopw \
-  -forever \
-  -shared \
-  -rfbport 5900 \
-  -bg
+echo "Starting VNC..."
+x11vnc -display :1 -nopw -forever -shared -rfbport 5900 -bg
 
 sleep 2
 
-echo "Launching Firefox (shared session)..."
+echo "Launching Firefox..."
 (
   sleep 6
-  dbus-launch /usr/bin/firefox \
+  dbus-launch firefox \
     --no-remote \
     --new-instance \
     --kiosk \
     https://rarestudy.in/
 ) &
 
-echo "Starting noVNC web server..."
+echo "Starting noVNC..."
 
 exec websockify \
   0.0.0.0:${PORT:-6080} \
